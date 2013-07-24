70fe82d5f5147242f89ca5080445f2b9905e5de1
Valerio Souza <eu@valeriosouza.com.br>
2013-07-23 22:03:01 -0300

    Commit Base Plugin

---
+++ live-broadcast.php
@@ -0,0 +1,60 @@
+<?php 
+/**
+ * Plugin Name: Live Broadcast
+ * Plugin URI: 
+ * Description: Plugin for streaming live for WordPress
+ * Author: Valerio Souza
+ * Author URI: http://valeriosouza.com.br
+ * Version: 0.1.0
+ * License: GPLv2 or later
+ * Text Domain: live_broad
+ * Domain Path: /lang/
+ */
+
+load_plugin_textdomain( 'live_broad', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
+
+require dirname( __FILE__ ) . '/loop-live-broadcast.php';
+
+@include_once 'loop-live-broadcast.php';
+
+add_action( 'init', 'create_post_type_live_broadcast' );
+
+function create_post_type_live_broadcast() {
+    register_post_type( 'live_broadcast',
+        array(
+            'labels' => array(
+                'name' => __( 'Live Broadcast','live_broad' ),
+                'singular_name' => __('Live Broadcast','live_broad' ),
+                'add_new' => __('New Post Live', 'live_broad'),
+                'add_new_item' => __('Add New Post Live','live_broad'),
+                'edit_item' => __('Edit Post Live','live_broad'),
+                'new_item' => __('New Post Live','live_broad'),
+                'all_items' => __('All Posts Live','live_broad'),
+                'not_found' => __('Write a First Transmission.','live_broad'),
+            ),
+            'public' => true,
+            'publicly_queryable' => true,
+            'exclude_from_search' => true,
+            'rewrite' => true,
+            'supports' => array( 'title','thumbnail'),
+            'has_archive' => true,
+            'menu_position' => 26,
+        )
+    );
+}
+
+
+add_filter('redirect_post_location', 'redirect_new_live_broadcast');
+
+function redirect_new_live_broadcast($location)
+{
+    if (isset($_POST['save']) || isset($_POST['publish'])) {
+        if (preg_match("/post=([0-9]*)/", $location, $match)) {
+            $pl = wp_redirect(admin_url('post-new.php?post_type=live_broadcast'));
+				die();
+            if ($pl) {
+                wp_redirect($pl);
+            }
+        }
+    }
+}
---
+++ loop-live-broadcast.php
@@ -0,0 +1,5 @@
+<?php
+/*
+Template Name:Show Live Broadcast Page
+*/
+
